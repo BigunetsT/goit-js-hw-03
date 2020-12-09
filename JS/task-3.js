cdb0d6455e9b991652791501bae92026009fc90a
Tetyana Bigunets <tanyuschamail83@gmail.com>
2020-12-09 21:33:04 +0200

    домашняя работа №3

---
+++ JS/task-3.js
@@ -0,0 +1,46 @@
+// Напиши функцию findBestEmployee(employees), которая принимает объект сотрудников и возвращает
+// имя самого продуктивного(который выполнил больше всех задач).
+// Сотрудники и кол - во выполненых задач содержатся как свойства объекта в формате "имя": "кол-во задач".
+
+const findBestEmployee = function (employees) {
+  const values = Object.values(employees);
+  const keys = Object.keys(employees);
+  let maxValue = values[0];
+  let bestEmployee = keys[0];
+  for (let i = 1; i < values.length; i += 1) {
+    if (values[i] > maxValue) {
+      maxValue = values[i];
+      bestEmployee = keys[i];
+    }
+  }
+  return bestEmployee;
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(
+  findBestEmployee({
+    ann: 29,
+    david: 35,
+    helen: 1,
+    lorence: 99,
+  }),
+); // lorence
+
+console.log(
+  findBestEmployee({
+    poly: 12,
+    mango: 17,
+    ajax: 4,
+  }),
+); // mango
+
+console.log(
+  findBestEmployee({
+    lux: 147,
+    david: 21,
+    kiwi: 19,
+    chelsy: 38,
+  }),
+); // lux
